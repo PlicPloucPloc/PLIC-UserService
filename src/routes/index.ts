cf6c90c100b54d9ecc64a15df5f9a67b3907b664
Babouche007 <baptiste.perrier@epita.fr>
2025-04-14 11:46:26 +0200

    fix: correct register

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { createUser } from "../services/authentication_service";
 
 const userRoutes = new Elysia({prefix: "/user"})
     .get("/", () => "Get all users")
-    .post("/register", ({ body }) => body, {
+    .post("/register", ({ body }) => createUser(body.email,body.password), {
         body : t.Object({
             firstName: t.String(),
             lastName: t.String(),
