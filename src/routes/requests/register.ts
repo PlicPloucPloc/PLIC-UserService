54a924fd62542aa3d63a8224a70fb5f3a975e4e1
Babouche <42094854+Babouche007@users.noreply.github.com>
2025-04-14 15:26:31 +0200

    feat: add login endpoint and logic (#2)
    * feat: add login endpoint and logic
    
    * fix: correct classes

--- src/routes/requests/register.ts
+++ src/routes/requests/register.ts
@@ -1,8 +1,15 @@
-type RegisterRequest = {
+class RegisterRequest {
     firstName: string;
     lastName: string;
     email: string;
     password: string;
+
+    constructor(firstName: string, lastName: string, email: string, password: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
 }
 
-export {RegisterRequest};
+export default RegisterRequest;
