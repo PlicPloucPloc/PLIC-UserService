dbbdcdbbcebd4eecfe1879e3fdcc68acb35e58fd
Ellmos <95100773+Ellmos@users.noreply.github.com>
2025-07-05 11:53:43 +0200

    Refactor db (#15)
    * update: refactor db model
    
    * update: added prettier

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,4 @@
-class User{
+class User {
     id: string;
     firstname: string;
     lastname: string;
--- src/services/info_service.ts
+++ src/services/info_service.ts
@@ -1,12 +1,12 @@
-import {getUserById, getAllUsers} from "../data/user";
-import User from "../models/user";
+import { getUserById, getAllUsers } from '../data/user';
+import User from '../models/user';
 
-async function userById(id: string) : Promise<User>{
-    return (await getUserById(id)); ;
+async function userById(id: string): Promise<User> {
+    return await getUserById(id);
 }
 
-async function allUsers() : Promise<User[]>{
+async function allUsers(): Promise<User[]> {
     return await getAllUsers();
 }
 
-export {userById, allUsers};
+export { userById, allUsers };
