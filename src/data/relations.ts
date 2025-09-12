9b95f9d60e9283c310a48db41b122fce45b2a95e
Babouche007 <baptiste.perrier@epita.fr>
2025-09-12 09:06:37 +0200

    Fix user node creation failed json parsing

--- src/data/relations.ts
+++ src/data/relations.ts
@@ -1,3 +1,5 @@
+import { HttpError } from "elysia-http-error";
+
 async function createUserNode(bearer: string): Promise<void> {
     const userUrl = (process.env.LIKE_URL || 'http://localhost:3000') + '/userNode';
     const request = new Request(userUrl, {
@@ -7,8 +9,13 @@ async function createUserNode(bearer: string): Promise<void> {
             Authorization: 'Bearer ' + bearer,
         },
     });
-    const aptID = (await (await fetch(request)).json()).apartment_id;
-    return aptID;
+    try {
+        await fetch(request);
+    }
+    catch (error) {
+        console.error('Error creating user node:', error);
+        throw new HttpError('Error creating user node', 500);
+    }
 }
 
 export { createUserNode };
